a3faa5c2635c625f93ea473c3ec003248eb25713
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-11-16 21:59:59 +0530

    Updated ArrangeNumberInAscendingOrder

--- SimpleJavaProoject/src/main/java/assignments/ArrangeNumberInAscendingOrder.java
+++ SimpleJavaProoject/src/main/java/assignments/ArrangeNumberInAscendingOrder.java
@@ -5,31 +5,46 @@ import java.util.Scanner;
 
 public class ArrangeNumberInAscendingOrder {
     public static void main(String[] args) {
-        int countDigit;
+        char flagToContinue ='Y';
         String input,newNumber="";
         int [] numArray;
         //System.in is a standard input stream
         Scanner sc= new Scanner(System.in);
 
-        //take the input as string
-        System.out.println("Enter the number: ");
-        input = sc.nextLine();
+        while (flagToContinue == 'Y'){
+            //take the input as string
+            System.out.println("Enter the number: ");
+            input = sc.nextLine();
+            if(isAlpha(input)==true){
+                System.out.println("Invalid entry,Add only numbers.Please try again.");
+            }else{   //converts string into arrays
+                numArray = numberInArray(input);
+
+                //arrange the arrays into ascending order
+                numArray = arrangeNumberInAscendingOrder(numArray);
+
+                //converts array into string and display number in ascending order
+                for(int i=0;i<numArray.length;++i){
+                    newNumber=newNumber+numArray[i];
+                }
+                System.out.println(input+" number arranged in ascending order as : " + newNumber);
+                flagToContinue ='N';
+            }
 
 
-        //converts string into arrays
-        numArray = numberInArray(input);
-
-        //arrange the arrays into ascending order
-        numArray = arrangeNumberInAscendingOrder(numArray);
-
-        //converts array into string and display number in ascending order
-        for(int i=0;i<numArray.length;++i){
-            newNumber=newNumber+numArray[i];
         }
-        System.out.println(input+" number arranged in ascending order as : " + newNumber);
 
     }
-
+    //to check if number entered as string has only numbers
+    static boolean isAlpha(String name) {
+        char[] chars = name.toCharArray();
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
     //arrange number in array
     static int[] arrangeNumberInAscendingOrder(int [] numberArray){
         int length = numberArray.length;
@@ -52,7 +67,6 @@ public class ArrangeNumberInAscendingOrder {
     //slice the number and store into array
     static int[] numberInArray(String input){
         int singleDigit=0;
-
         int [] array = new int[input.length()];
             for(int i=0;i<input.length();i++){
                 //parse the number in the string into integer number
@@ -63,5 +77,9 @@ public class ArrangeNumberInAscendingOrder {
         return array;
     }
 
-
+    //counts the number of digits in a number string
+    /*static int countDigitInNumber(String input){
+        int count =0;
+        return count =input.length();
+    }*/
 }
