333bceccf3f9c747f51ab55bb793308fcfd99b56
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-11-18 19:50:23 +0530

    added maven sample project

---
+++ MavenSampleProject/src/test/java/org/example/mavenproject/AppTest.java
@@ -0,0 +1,26 @@
+package org.example.mavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("I am into Testing mode");
+
+    }
+    @Test
+    public void testWithMe(){
+        System.out.println("Not to be tested");
+    }
+}
