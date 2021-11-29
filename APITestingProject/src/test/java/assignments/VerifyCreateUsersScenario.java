8a61c08ffbb7f1652b6dfa26696418a18acdc5f1
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-11-29 18:25:55 +0530

    added verification of creation of user

--- APITestingProject/src/test/java/assignments/VerifyCreateUsersScenario.java
+++ APITestingProject/src/test/java/assignments/VerifyCreateUsersScenario.java
@@ -3,6 +3,7 @@ package assignments;
 import assignments.data.UserCreationResponseDetails;
 import assignments.data.UserDataClass;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class VerifyCreateUsersScenario {
     @Test
@@ -31,6 +33,7 @@ public class VerifyCreateUsersScenario {
         userOneDataDetails.setPassword("123456");
         userOneDataDetails.setPhone("444-00-444");
         userOneDataDetails.setUserStatus(0);
+        String userNameOne = userOneDataDetails.getUsername();
         userList.add(userOneDataDetails);
 
         //second user object data
@@ -42,6 +45,7 @@ public class VerifyCreateUsersScenario {
         userTwoDataDetails.setPassword("123456");
         userTwoDataDetails.setPhone("444-060-444");
         userTwoDataDetails.setUserStatus(0);
+        String userNameTwo = userTwoDataDetails.getUsername();
         userList.add(userTwoDataDetails);
 
         userCreationResponseDetails =
@@ -56,8 +60,34 @@ public class VerifyCreateUsersScenario {
         System.out.println("Checking response after users creation: "+userCreationResponseDetails.toString());
         Assert.assertEquals(200,userCreationResponseDetails.getCode());
 
+        //verify that above userOne is created using GET method, using username
+        String userOneDetails = given().log().all().
+                when().get("/v2/user/"+userNameOne).then().assertThat().log().all().
+                body("username",equalTo(userNameOne)).
+                body("firstName",equalTo(userOneDataDetails.getFirstName())).statusCode(200).
+                extract().response().asString();
+
+        JsonPath userOneDetailsJsonPath = new JsonPath(userOneDetails);
+        //verify lastname from response equals to lastname fetched in response of GET method
+        String userOnelastName = userOneDetailsJsonPath.getString("lastName");
+        Assert.assertEquals(userOneDataDetails.getLastName(),userOnelastName);
+
+
+
+        //verify that above userTwo is created using GET method, using username
+        String userTwoDetails = given().log().all().
+                when().get("/v2/user/"+userNameTwo).then().assertThat().log().all().
+                body("username",equalTo(userNameTwo)).
+                body("firstName",equalTo(userTwoDataDetails.getFirstName())).statusCode(200).
+                extract().response().asString();
+
+        JsonPath userTwoDetailsJsonPath = new JsonPath(userTwoDetails);
+        //verify lastname from response equals to lastname fetched in response of GET method
+        String lastNameUserTwo = userTwoDetailsJsonPath.getString("lastName");
+        Assert.assertEquals(userTwoDataDetails.getLastName(),lastNameUserTwo);
+
+
 
     }
-
 
 }
