e7d811f019218e820f829096f3fc76f8d4b084b3
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2022-01-10 10:22:59 +0530

    Added sample code for various exercise

---
+++ src/main/java/org/example/ScannerSample.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+       double d = scan.nextDouble();
+        scan.nextLine();
+        String s=scan.nextLine();
+       // s = s+scan.nextLine();
+
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
+
